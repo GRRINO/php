50677a68a67d04e0b44453692f096383bd92892f
GRRINO <yy79250@gmail.com>
2025-05-10 14:58:57 +0630

    first commit

---
+++ php_basic/variable.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = "Yan Yan";
+$age = 20;
+$height = 5.8;
+$isStudent = true;
+echo $height;
+
+$fruits = ["apple","orange","peach","kiwi"];
+$fruitLists = array("apple","orange","peach","kiwi");
+
+// var_dump($fruits);
+// print_r($fruitLists);
+
+echo $fruits[0];
+
+?>
